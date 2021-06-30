ac85eb451b74c95dd5bbf217f13a32b4563cced6
cknig <78619490+cknig00q@users.noreply.github.com>
2021-06-29 20:50:45 -0700

    adds db models and env example

---
+++ models/Activities.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Activities extends Model {}
+
+Activities.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+    indoor_outdoor: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    food: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    drink: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'activities',
+  }
+);
+
+module.exports = Activities;
