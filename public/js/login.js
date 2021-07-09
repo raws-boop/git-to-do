fb26687b134c548a536798be38350b0d0e5ce682
alicewaweru28 <alicewaweru28@gmail.com>
2021-07-09 14:02:18 -0700

    merge chages

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const router = express.Router();
- 
-//login handle
-router.get('/login',(req,res)=>{
-   res.render('login');
-})
-router.get('/register',(req,res)=>{
-   res.render('register')
-   })
-//Register handle
-router.post('/register',(req,res)=>{
-})
-router.post('/login',(req,res,next)=>{
- })
- 
-//logout
-router.get('/logout',(req,res)=>{
-})
-module.exports  = router;
+function setFrormMessage(formElement, type, message) {
+    const message = formElement.querySelector('.form__message');
+
+    messageElemenet.textContent = message;
+    messageElemenet.classList.remove("form__message--success", "form__message--error");
+    messageElemenet.classList.add('form__messsage--${type}');
+}
+
+setFormMessage(loginForm, "success", "You'rs logged in!");
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.addEventListener("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+document.querySelector("#linkLogin").addEventListener("click", e => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+});
+
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    //Perform your AJAX/Fetch login
+
+    setFormMessage(loginform, "error", "Invalid username/password combination");
+  });
+});
